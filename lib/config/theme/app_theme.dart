d0cb8ccb380c3aaa1c4a328a6147edbea0532dd8
JaViKo <javikogutierrez64@gmail.com>
2023-05-15 23:43:37 -0500

    selected theme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 
+const Color _customColor = Color(0xFF49143F);
+
+List<Color> _colorThemes = [
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
+  Colors.black,
+];
+
 class AppTheme {
+  final int selectedColor;
+
+  AppTheme({ required this.selectedColor})
+    : assert( selectedColor >= 0, 'Colors must be between 0 and ${ _colorThemes.length - 1 }' ),
+      assert( selectedColor <= 7, 'Colors must be between 0 and ${ _colorThemes.length - 1 }' );
+
   ThemeData theme() {
-    return ThemeData(
-      useMaterial3: true
-    );
+    return ThemeData(useMaterial3: true, colorSchemeSeed: _colorThemes[selectedColor]);
   }
 }
