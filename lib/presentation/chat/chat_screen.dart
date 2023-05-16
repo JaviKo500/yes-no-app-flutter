7aec1cf2f8cd9a6d82037ad6d4ba540c0a76f84b
JaViKo <javikogutierrez64@gmail.com>
2023-05-16 00:04:19 -0500

    listview

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
-   
   const ChatScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,18 +11,36 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
             backgroundImage: NetworkImage(
-              'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fd16c84d-dbba-4d3a-a4e3-14d52f3fd6aa/df6o8wh-690f5f13-347e-4376-8100-4eac9706b683.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2ZkMTZjODRkLWRiYmEtNGQzYS1hNGUzLTE0ZDUyZjNmZDZhYVwvZGY2bzh3aC02OTBmNWYxMy0zNDdlLTQzNzYtODEwMC00ZWFjOTcwNmI2ODMuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pXZrphw82DUSgE8cKaAJj8wDzPBXZ4dWuBcW4fLCCRs'
-            ),
+                'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fd16c84d-dbba-4d3a-a4e3-14d52f3fd6aa/df6o8wh-690f5f13-347e-4376-8100-4eac9706b683.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2ZkMTZjODRkLWRiYmEtNGQzYS1hNGUzLTE0ZDUyZjNmZDZhYVwvZGY2bzh3aC02OTBmNWYxMy0zNDdlLTQzNzYtODEwMC00ZWFjOTcwNmI2ODMuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pXZrphw82DUSgE8cKaAJj8wDzPBXZ4dWuBcW4fLCCRs'),
           ),
         ),
         centerTitle: true,
-        title: const Text(
-          'UN Kokun',
-          style: TextStyle()
-        ),
+        title: const Text('UN Kokun', style: TextStyle()),
       ),
-      body: const Center(
-         child: Text('ChatScreen'),
+      body: const _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('index: $index', style: const TextStyle());
+              },
+            )),
+            const Text('Hello', style: TextStyle()),
+          ],
+        ),
       ),
     );
   }
