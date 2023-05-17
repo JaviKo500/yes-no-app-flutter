bd4f7af11d04b708350fa8822db5eae3f129f1c9
JaViKo <javikogutierrez64@gmail.com>
2023-05-16 21:39:19 -0500

    image network

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -40,13 +40,24 @@ class _ImageBubble extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Image.network(
-        'https://yesno.wtf/assets/no/23-5fe6c1ca6c78e7bf9a7cf43e406fb8db.gif',
-        width: size.width * 0.7,
-        height: 150,
-        fit: BoxFit.cover,
-      )
-    );
+        borderRadius: BorderRadius.circular(20),
+        child: Image.network(
+          'https://yesno.wtf/assets/no/23-5fe6c1ca6c78e7bf9a7cf43e406fb8db.gif',
+          width: size.width * 0.7,
+          height: 150,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+            return Container(
+              width: size.width * 0.7,
+              height: 150,
+              padding: const EdgeInsets.symmetric( horizontal: 10, vertical: 5),
+              child: const Text(
+                'Kokun writing',
+                style: TextStyle()
+              ),
+            );
+          },
+        ));
   }
 }
