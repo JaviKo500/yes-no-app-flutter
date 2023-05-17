83622453b6e78965efabb59d0ab2365b21f3b059
JaViKo <javikogutierrez64@gmail.com>
2023-05-17 05:51:29 -0500

    show messages provider

--- lib/presentation/widgets/chat/my_message_bubble.dart
+++ lib/presentation/widgets/chat/my_message_bubble.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
 
 class MyMessageBubble extends StatelessWidget {
-  const MyMessageBubble({Key? key}) : super(key: key);
+  final Message message;
+  const MyMessageBubble({
+    required this.message,
+    Key? key
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
-    
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         Container(
           decoration: BoxDecoration(
               color: colors.primary, borderRadius: BorderRadius.circular(20)),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text('Cupidatat laborum labore dolore.',
-                style: TextStyle(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(message.text,
+                style: const TextStyle(
                   color: Colors.white,
                 )),
           ),
